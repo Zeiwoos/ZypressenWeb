ef299b606d4bc06c295cf03f79a45e1d8caa5773
Osprey <u202211707@hust.edu.cn>
2025-01-21 19:09:14 +0800

    project initialization

---
+++ zypressen_back/src/main/java/com/neutech/zypressen_back/ZypressenBackApplication.java
@@ -0,0 +1,13 @@
+package com.neutech.zypressen_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZypressenBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZypressenBackApplication.class, args);
+    }
+
+}
---
+++ zypressen_back/src/test/java/com/neutech/zypressen_back/ZypressenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutech.zypressen_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZypressenBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
